31200b41abdcb02eddb092920ece4d27d7598b27
tobias <tobias.hoelzer@gmail.com>
2024-06-09 20:56:22 +0200

    adding single and compare mode. adding scolling on mobile. adding current time indicator

--- fit.py
+++ fit.py
@@ -1,17 +1,21 @@
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from datetime import datetime
 
 # These were calculated from data
 PARAMS = (4.816931830999022, 0.23815199140828144, 0.7144975482181155)
+
 
 # Define the bi-exponential model
 def bi_exponential(t, Cmax, Ka, Ke):
     return (Cmax * Ka / (Ka - Ke)) * (np.exp(-Ke * t) - np.exp(-Ka * t))
 
+
 # Function to calculate model predictions using the fitted parameters
 def predict_concentration(t, Cmax, Ka, Ke, dose):
     return bi_exponential(t, Cmax, Ka, Ke) * dose
+
 
 # Function to calculate total and individual concentrations over time
 def total_and_individual_concentration_over_time(dose_times, params):
@@ -35,15 +39,23 @@ def total_and_individual_concentration_over_time(dose_times, params):
 
     return time_array, total_concentration, individual_concentrations
 
+
 # Function to calculate concentrations and plot with Plotly
 def calculate_concentrations_and_plot_with_plotly(dose_time_options, threshold=30):
-    fig = make_subplots(rows=1, cols=1)
+    # fig = make_subplots(rows=1, cols=1)
+    fig = fig = go.Figure(
+        # layout=dict(autosize=False,
+        #             width=2000,
+        # height=600,
+        # margin=dict(l=50, r=50, b=50, t=50, pad=4),
+        # )
+    )
 
     for i, dose_times in enumerate(dose_time_options, 1):
         time_array, total_concentration, individual_concentrations = (
             total_and_individual_concentration_over_time(dose_times, PARAMS)
         )
-        
+
         # Adding individual concentration traces
         for j, concentration in enumerate(individual_concentrations, start=1):
             fig.add_trace(
@@ -54,25 +66,68 @@ def calculate_concentrations_and_plot_with_plotly(dose_time_options, threshold=3
                     name=f"{dose_times[j-1][1]}mg",
                     line=dict(dash="dot", color="darkgrey"),
                 ),
-                row=1,
-                col=1,
+                # row=1,
+                # col=1,
             )
-        
+
         # Adding total concentration trace
+        if i == 1:
+            name = "Total Concentration"
+        else:
+            name = f"Total Concentration Option {i}"
         fig.add_trace(
             go.Scatter(
                 x=time_array,
                 y=total_concentration,
                 mode="lines",
-                name=f"Total Concentration Option {i}",
+                name=name,
                 line=dict(width=2),
             ),
-            row=1,
-            col=1,
+            # row=1,
+            # col=1,
         )
 
     # Adding a horizontal line for the threshold concentration
-    fig.add_hline(y=threshold, line_color="black", line_width=2, row=1, col=1)
+    fig.add_hline(
+        y=threshold,
+        line_color="black",
+        line_width=2,
+        #   row=1, col=1
+    )
+    # Adding a vertical rectangle to highlight the current time
+    time_array, total_concentration, individual_concentrations = (
+        total_and_individual_concentration_over_time(dose_time_options[0], PARAMS)
+    )
+    hour = datetime.now().hour
+    minutes = datetime.now().minute
+    xtime = hour + (minutes / 60)
+    delta = 0.25
+    # get y coordinate of xtime using numpy:
+    xindex = np.searchsorted(time_array, xtime)
+    ytime = total_concentration[xindex]
+
+    fig.add_vrect(
+        x0=xtime - delta,
+        x1=xtime + delta,
+        #   annotation_text="decline", annotation_position="top left",
+        fillcolor="green",
+        opacity=0.25,
+        line_width=0,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=[xtime],
+            y=[ytime],
+            mode="markers",
+            marker=dict(
+                size=12,
+                #    color=None,
+                color="rgba(191,223,191, 0.5)",
+                line=dict(width=2, color="DarkSlateGrey"),
+            ),
+            name="you are here",
+        )
+    )
 
     # Updating layout to make it look nice
     fig.update_layout(
@@ -80,7 +135,7 @@ def calculate_concentrations_and_plot_with_plotly(dose_time_options, threshold=3
         xaxis_title="Hour of the Day",
         yaxis_title="Concentration (ng/mL)",
         legend_title="Concentration Type",
-        xaxis=dict(range=[5, 24], dtick=1),  # Hourly ticks from 5 to 24
+        xaxis=dict(range=[5, 24], dtick=1, showgrid=True),  # Hourly ticks from 5 to 24
         template="plotly_white",
     )
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,32 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from datetime import time
+from datetime import time, datetime
 from fit import calculate_concentrations_and_plot_with_plotly
+import streamlit.components.v1 as components
+from streamlit_javascript import st_javascript
+
+
+window_width = st_javascript("""window.innerWidth;""")
+st.session_state.is_mobile = True if window_width < 600 else False
+st.session_state.current_hour = datetime.now().hour
 
 # Application Title
-st.title("Elvanse Medication Calculator")
+st.title("Medication Buddy")
 
 # Introduction Text
-st.markdown("""
+st.markdown(
+    """
 So you really have something important at 4pm?
 When should you take your medication? 
 Should it be one 70mg or two 30mg?
 
 Try it out!
 ## When did you take your medication and how much?
-""")
+"""
+)
+
+tab1, tab2 = st.tabs(["My Medication", "Compare"])
 
 # Define the DataFrames
 df1 = pd.DataFrame(
@@ -30,7 +41,6 @@ df2 = pd.DataFrame(
         {"time": time(7, 30), "dosis [mg]": 70},
     ]
 )
-
 # Define the Column Configuration for the Data Editors
 column_config = {
     "time": st.column_config.TimeColumn(
@@ -39,7 +49,7 @@ column_config = {
         required=True,
         min_value=time(0, 0),
         max_value=time(23, 59),
-        format="HH:mm"
+        format="HH:mm",
     ),
     "dosis [mg]": st.column_config.NumberColumn(
         "Dosage [mg]",
@@ -48,20 +58,34 @@ column_config = {
         min_value=0,
         max_value=200,
         step=5,
-        format="%d"
-    )
+        format="%d",
+    ),
 }
+with tab1:
+    edited_df1 = st.data_editor(
+        df1,
+        column_config=column_config,
+        num_rows="dynamic",
+        hide_index=True,
+        key="default",
+    )
 
-# Layout for Data Editors
-col1, col2 = st.columns(2, gap="small")
+with tab2:
+    # Layout for Data Editors
+    col1, col2 = st.columns(2, gap="small")
 
-with col1:
-    st.markdown("### Option 1")
-    edited_df1 = st.data_editor(df1, column_config=column_config, num_rows="dynamic", hide_index=True)
+    with col1:
+        st.markdown("### Option 1")
+        edited_df1 = st.data_editor(
+            df1, column_config=column_config, num_rows="dynamic", hide_index=True
+        )
 
-with col2:
-    st.markdown("### Option 2")
-    edited_df2 = st.data_editor(df2, column_config=column_config, num_rows="dynamic", hide_index=True)
+    with col2:
+        st.markdown("### Option 2")
+        edited_df2 = st.data_editor(
+            df2, column_config=column_config, num_rows="dynamic", hide_index=True
+        )
+        compare_mode = st.checkbox("Mode: Compare", value=False, key="compare_mode")
 
 # Layout for Medication Selection and Threshold Slider
 col3, col4 = st.columns([0.7, 0.3])
@@ -70,42 +94,68 @@ with col3:
     medication = st.radio(
         "Elvanse, Medikinet, or ...?",
         ["Elvanse"],
-        captions=["Lisdexamphetamindimesilat"]
+        captions=["Lisdexamphetamindimesilat"],
     )
     st.markdown("More Medications coming soon")
 
 with col4:
-    threshold = st.slider("Personal Threshold", min_value=0, max_value=200, value=20, step=10)
+    threshold = st.slider(
+        "Personal Threshold", min_value=0, max_value=200, value=20, step=10
+    )
+
 
 # Function to Extract Options from DataFrame
 def get_options(df):
-    times = df['time'].apply(lambda x: x.hour + (x.minute / 60)).tolist()
+    times = df["time"].apply(lambda x: x.hour + (x.minute / 60)).tolist()
     doses = df["dosis [mg]"].tolist()
     return [[t, d] for t, d in zip(times, doses)]
+
 
 # Get Options from Edited DataFrames
 options1 = get_options(edited_df1)
 options2 = get_options(edited_df2)
+plot_options = [options1]
+if compare_mode:
+    plot_options.append(options2)
 
 # Calculate Concentrations and Plot
-fig1 = calculate_concentrations_and_plot_with_plotly([options1, options2], threshold)
+fig1 = calculate_concentrations_and_plot_with_plotly(plot_options, threshold)
 
-fig1.update_layout(
-    height=600,
-    legend=dict(
-        orientation="h",
-        yanchor="bottom",
-        y=-0.4,
-        xanchor="center",
-        x=0.5
+if st.session_state.is_mobile:
+    # factor = 300./float(window_width)
+    # print(factor)
+    legend = dict(orientation="h", yanchor="bottom", y=-1.9, xanchor="center", x=0.5)
+    h = st.session_state.current_hour
+    delta = 2.2
+    xrange = [h - delta, h + delta]
+    xaxis = dict(
+        range=xrange,
+        rangeslider=dict(
+            visible=True,
+        ),
+        type="linear",
     )
+else:
+    legend = dict(orientation="h", yanchor="bottom", y=-0.9, xanchor="center", x=0.5)
+    xaxis = dict(
+        # range=range,
+        # rangeslider=dict(
+        #     visible=True,
+        # ),
+        type="linear"
+    )
+fig1.update_layout(
+    dragmode="pan",
+    legend=legend,
+    xaxis=xaxis,
 )
 
-# Display Plot
-st.plotly_chart(fig1, use_container_width=True, theme="streamlit")
+# st.markdown("**Total and Individual Medication Concentrations Over Time**")
+st.plotly_chart(fig1, use_container_width=False, theme="streamlit")
 
 # Add CSS to make it nicer on mobile
-st.markdown("""
+st.markdown(
+    """
 <style>
     .stApp {
         max-width: 100% !important;
@@ -123,4 +173,6 @@ st.markdown("""
         }
     }
 </style>
-""", unsafe_allow_html=True)
+""",
+    unsafe_allow_html=True,
+)
